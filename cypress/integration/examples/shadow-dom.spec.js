6db1aeef596f360e721cfaf759e5f42f35597778
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-28 10:37:45 +0100

    ✅ reproduce Cypress Shadow DOM snapshot issue

--- cypress/integration/examples/shadow-dom.spec.js
+++ cypress/integration/examples/shadow-dom.spec.js
@@ -2,4 +2,9 @@ context('ShadowDOM', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080');
   });
+  it('should read Shadow DOM content', () => {
+    cy.get('#content')
+      .shadow()
+      .should('contain', 'Hey Cypress! Can you read this?');
+  });
 });
